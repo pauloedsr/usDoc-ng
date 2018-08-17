ff20923df0c3eed2c941f31e20b78c785298832a
pauloedsr <p.eduardo@jackdoo.com.br>
2018-08-16 23:12:09 -0300

    interface projeto

--- src/app/shared/services/us.service.ts
+++ src/app/shared/services/us.service.ts
@@ -1,3 +1,4 @@
+import { ProjetosRestI } from './../us-doc.ed';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -10,10 +11,10 @@ export class UsService {
 
   constructor(private http: HttpClient) { }
 
-  getProjetos(): Observable<any> {
+  getProjetos(): Observable<ProjetosRestI[]> {
     return Observable.create(observer => {
       const autor = localStorage.getItem('id');
-      this.http.get(`${SERVER_URL}/projeto/list/${autor}`).subscribe(data => {
+      this.http.get(`${SERVER_URL}/projeto/list/${autor}`).subscribe((data: ProjetosRestI[]) => {
         observer.next(data);
       });
     });
---
+++ src/app/shared/us-doc.ed.ts
@@ -0,0 +1,9 @@
+export interface ProjetosRestI {
+    _id: string;
+    updatedAt: string;
+    createdAt: string;
+    autor: string;
+    nome: string;
+    descricao: string;
+    __v: number;
+}
